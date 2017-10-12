9bd31bd1a66beaaac1f345b0c75bf20056071732
Kishan Kumar <Kishan.javatrainer@gmail.com>
2017-10-12 23:04:03 +0530

    code

---
+++ ManyToManyMappingExample/src/main/java/com/infotech/client/FetchDataClientTest.java
@@ -0,0 +1,27 @@
+package com.infotech.client;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.infotech.entities.Employee;
+import com.infotech.util.HibernateUtil;
+
+public class FetchDataClientTest {
+
+	public static void main(String[] args) {
+		getEmployeeAndAdressByEmployeeId();
+	    
+	  }
+
+	private static void getEmployeeAndAdressByEmployeeId() {
+	    try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+	    	Employee employee2 = session.get(Employee.class , 1);
+	    	System.out.println(employee2);
+	    	if(employee2 != null){
+	    		employee2.getAddressList().forEach(System.out::println);
+	    	}
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+	}
+}
